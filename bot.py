cfbd19551b46f26095bcc73ed6f9ff7d9521d840
deformator852 <kostneko30@gmail.com>
2024-07-25 08:19:24 +0000

    changes

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import requests
 
 TOKEN = "7122908765:AAFQCJwn-_0Rtn6GBp5hbOxi8SoYiLauTZg"
-ROOT_ADMIN = 6432444926  # it's me
-#ROOT_ADMIN = 1355559290
+IM = 6432444926  # it's me
+ROOT_ADMIN = 1355559290
 
 
 def send_load(load_price, load_id):
@@ -12,7 +12,7 @@ def send_load(load_price, load_id):
     payload = {"chat_id": ROOT_ADMIN, "text": text}
     response = requests.post(url, data=payload)
     if response.status_code != 200:
-        # send_load_me(load_price, load_id)
+        send_load_me(load_price, load_id)
         print("Error: ", response.status_code)
 
 
